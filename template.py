8d32d20174dc96e243bd4f3a9e291bc7cb245533
jihaad2021 <jihaadariefpangestu@gmail.com>
2024-07-06 19:57:33 +0700

    folder structure added

---
+++ template.py
@@ -0,0 +1,51 @@
+import os
+from pathlib import Path
+import logging 
+
+logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s:') 
+
+project_name = "WasteDetection"
+
+list_of_files = [
+    ".github/workflows'/.gitkeep",
+    "data/.gitkeep",
+    f"{project_name}/__init__.py",
+    f"{project_name}/components/__init__.py",
+    f"{project_name}/components/data_ingestion.py",
+    f"{project_name}/components/data_validation.py",
+    f"{project_name}/components/model_trainer.py",
+    f"{project_name}/constant/__init__.py",
+    f"{project_name}/constant/training_pipeline/__init__.py",
+    f"{project_name}/constant/application.py",
+    f"{project_name}/entity/__init__.py",
+    f"{project_name}/entity/config_entity.py",
+    f"{project_name}/entity/artifacts_entitiy.py",
+    f"{project_name}/exeption/__init__.py",
+    f"{project_name}/logger/__init__.py",
+    f"{project_name}/pipeline/__init__.py",
+    f"{project_name}/pipeline/training_pipeline.py",
+    f"{project_name}/utils/__init__.py",
+    f"{project_name}/utils/main_utils.py",
+    "templates/index.html",
+    "app.py",
+    "Dockerfile",
+    "recuirements.txt",
+    "setup.py"
+]
+
+for filepath in list_of_files:
+    filepath = Path(filepath) # digunakan untuk merubah filepath menjadi format yang sesuai os yang add ex linux
+    
+    filedir, filename = os.path.split(filepath)
+    
+    if filedir!="":
+        os.makedirs(filedir, exist_ok=True)
+        logging.info(f"Creating directory: {filedir} for the {filename}")
+        
+    if (not os.path.exists(filename)) or (os.path.getsize(filename) == 0):
+        with open(filepath, "w") as f:
+            pass
+            logging.info(f"Creating empty file: (filename)")
+            
+    else:
+        logging.info(f"{filename} is already created")
